dbe25d293819a22c7a06d532b61a3e01f0b403e5
brianasantander <98927180+brianasantander@users.noreply.github.com>
2022-02-16 20:59:25 -0500

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,21 @@
+average = 0
+total = 0
+howManyEntered = 0
+ print ("How many test scores would you like to average?")
+howMany = int(input(''))
+ print ("enter test score")
+testScore = int(input(""))
+total = testScore + total
+total = 1 + howManyEntered
+i = how ManyEntered
+while how ManyEntered < howMany
+
+average = total/howMany
+ print('The average for the test scores you entered is:')
+ print('average')
+ 
+ 
+
+
+
+
